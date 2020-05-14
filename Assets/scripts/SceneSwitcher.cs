b8612a115ce28ab114bfa3978d0a8f92563cabaf
yuvarathna <yuvarathna9999@gmail.com>
2020-05-14 15:25:59 +0530

    initial commit

---
+++ Assets/scripts/SceneSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void next()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    void previous()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
