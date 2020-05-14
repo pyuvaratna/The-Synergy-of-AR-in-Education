a9aeb899ded196f28d3c1089c0792f809aed79f2
yuvarathna <yuvarathna9999@gmail.com>
2020-05-14 15:44:27 +0530

    added files

---
+++ LOAD_SCENE_withLOADINgSlider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class loadScene : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    public void SceneLoader(int sceneindex)
+    {
+        StartCoroutine(AsynchronousLoad(sceneindex));
+    }
+    IEnumerator AsynchronousLoad(int sceneindex)
+    {
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(sceneindex);
+        loadingScreen.SetActive(true);
+        
+
+        while (!ao.isDone)
+        {
+            
+            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+           
+
+            yield return null;
+        }
+    }
+
+}
+
